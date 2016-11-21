d31e74ff4e55edda0952fbcf347b14042bd65312
michaeltan <michaeltan@192.168.99.1>
2016-11-21 14:43:11 +0800

    delete

--- src/main/java/businesslogicservice/customerblservice/CustomerInfoblService.java
+++ src/main/java/businesslogicservice/customerblservice/CustomerInfoblService.java
@@ -4,10 +4,8 @@ import vo.CustomerVO;
 
 public interface CustomerInfoblService {
 	
-	//得到所有客户的列表
 	public CustomerVO[] getInfoList();
 	
-	//得到某客户的信息
 	public CustomerVO getInfo(String customerID);
 
 }
--- src/main/java/businesslogicservice/customerblservice/VIPblService.java
+++ src/main/java/businesslogicservice/customerblservice/VIPblService.java
@@ -5,7 +5,6 @@ import po.LevelPO;
 
 public interface VIPblService {
 	
-	//使该账号注册会员
 	public void registerVIP(CustomerPO po);
 	
 	public void updateLevel(CustomerPO cpo, LevelPO lpo);
