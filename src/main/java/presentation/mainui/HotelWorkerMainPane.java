503813241726ff4d9b91209581a246e2c4489785
samperson1997 <2474809464@qq.com>
2016-12-12 19:48:37 +0800

    2016/12/12 19:48 szs 合并酒店工作人员查看订单和办理线上入住功能

--- src/main/java/presentation/mainui/HotelWorkerMainPane.java
+++ src/main/java/presentation/mainui/HotelWorkerMainPane.java
@@ -7,7 +7,6 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.VBox;
 import presentation.hotelui.WorkerHotelInfoPane;
 import presentation.orderui.HotelOrdersPane;
-import presentation.orderui.ProducingOrderDialog;
 import presentation.orderui.ProducingOrderOfflineDialog;
 import presentation.promotionui.HotelPromotionPanel;
 
@@ -23,22 +22,11 @@ public class HotelWorkerMainPane extends VBox {
 		Button manageButton = new Button("酒店基本信息维护");
 		Button hotelPromotionButton = new Button("酒店促销策略制定");
 		Button offlineButton = new Button("线下入住与退房办理");
-		Button onlineButton = new Button("线上入住与退房办理");
-		Button orderListButton = new Button("查看订单");
+		Button orderListButton = new Button("线上入住与退房办理");
 		Button logoutButton = new Button("注销登录");
 
-		
-		
-		
-		this.getChildren().addAll(manageButton, hotelPromotionButton, offlineButton, onlineButton, orderListButton,
-				logoutButton);
+		this.getChildren().addAll(manageButton, hotelPromotionButton, offlineButton, orderListButton, logoutButton);
 
-		
-		
-		
-		
-		
-		
 		manageButton.addEventHandler(MouseEvent.MOUSE_CLICKED, (event) -> {
 			try {
 				TheMainFrame.jumpTo(new WorkerHotelInfoPane(hotelID));
@@ -67,10 +55,6 @@ public class HotelWorkerMainPane extends VBox {
 			}
 		});
 
-		onlineButton.addEventHandler(MouseEvent.MOUSE_CLICKED, (event) -> {
-			// TheMainFrame.jumpTo();
-		});
-
 		orderListButton.addEventHandler(MouseEvent.MOUSE_CLICKED, (event) -> {
 			try {
 				TheMainFrame.jumpTo(new HotelOrdersPane(hotelID));
@@ -82,8 +66,7 @@ public class HotelWorkerMainPane extends VBox {
 		logoutButton.addEventHandler(MouseEvent.MOUSE_CLICKED, (event) -> {
 			TheMainFrame.backTo();
 		});
-		
-		
+
 		this.getStylesheets().add(getClass().getResource("HotelWorkerMainPane.css").toExternalForm());
 	}
 }
