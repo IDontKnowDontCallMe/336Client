6658581862109f85727260df2bbb3ba006803ab6
TXin96 <609012488@qq.com>
2016-12-18 20:38:28 +0800

    merge

--- src/main/java/presentation/hotelui/HotelListPane.java
+++ src/main/java/presentation/hotelui/HotelListPane.java
@@ -4,11 +4,11 @@ import java.rmi.RemoteException;
 import java.util.List;
 import bussinesslogic.factory.BLFactory;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
-import javafx.scene.text.Text;
 import presentation.mainui.TheMainFrame;
 import presentation.orderui.ProducingOrderDialog;
 import vo.HotelVO;
@@ -58,12 +58,18 @@ public class HotelListPane extends ScrollPane {
 				break;
 			}
 
-			this.add(new Text(hotelVO.hotelName), 1, 0, 1, 1);
-			this.add(new Text(String.valueOf(hotelVO.commentScore) + "分"), 2, 0, 1, 1);
-			this.add(new Text(booked), 4, 1, 1, 1);
-			this.add(new Text(String.valueOf(hotelVO.score) + "星级"), 3, 0, 1, 1);
-			this.add(new Text("¥" + String.valueOf(hotelVO.minPrice) + "起"), 4, 0, 1, 1);
-			this.add(new Text(String.valueOf(hotelVO.address)), 1, 1, 3, 1);
+			Label hotelName = new Label(hotelVO.hotelName);
+			this.add(hotelName, 1, 0, 1, 1);
+			Label commentScore = new Label(String.valueOf(hotelVO.commentScore)+"分");
+			this.add(commentScore, 2, 0, 1, 1);
+			Label ifbooked = new Label(booked);
+			this.add(ifbooked, 4, 1, 1, 1);
+			Label star = new Label(String.valueOf(hotelVO.score) + "星级");
+			this.add(star, 3, 0, 1, 1);
+			Label minPrice = new Label("¥" + String.valueOf(hotelVO.minPrice) + "起");
+			this.add(minPrice, 4, 0, 1, 1);
+			Label address = new Label(String.valueOf(hotelVO.address));
+			this.add(address, 1, 1, 3, 1);
 
 			produceButton = new Button("下订单");
 			this.add(produceButton, 5, 1, 1, 1);
@@ -90,6 +96,7 @@ public class HotelListPane extends ScrollPane {
 					e.printStackTrace();
 				}
 			});
+			this.getStylesheets().add(getClass().getResource("SimpleHotelCell.css").toExternalForm());
 		}
 	}
 
