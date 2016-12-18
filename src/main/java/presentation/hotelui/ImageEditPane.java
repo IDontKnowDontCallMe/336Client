8f2cad1d1fe01b4cafe4191353dec74ad9e0cde6
samperson1997 <2474809464@qq.com>
2016-12-19 00:20:17 +0800

    2016/12/19 00:19 szs 酒店工作人员可以编辑酒店图片

--- src/main/java/presentation/hotelui/ImageEditPane.java
+++ src/main/java/presentation/hotelui/ImageEditPane.java
@@ -15,24 +15,24 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.FileChooser;
 
-public class ImageEditPane extends VBox{
-	
+public class ImageEditPane extends VBox {
+
 	private int hotelID;
-	
+
 	private ImageView imageView;
 	private Image preImage;
 	private Image transitionImage;
 	private byte[] transitionImageData;
-	
+
 	private HBox editButtonBox;
 	private Button chooseButton;
 	private Button confirmButton;
 	private Button cancelButton;
-	
-	public ImageEditPane(int hotelID) throws RemoteException{
+
+	public ImageEditPane(int hotelID) throws RemoteException {
 		super();
 		this.hotelID = hotelID;
-		
+
 		byte[] imageData = BLFactory.getInstance().getHotelBLService().getHotelImage(hotelID);
 		ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(imageData);
 		preImage = new Image(byteArrayInputStream);
@@ -42,21 +42,22 @@ public class ImageEditPane extends VBox{
 		imageView.setPreserveRatio(false);
 		imageView.setFitHeight(200);
 		imageView.setFitWidth(300);
-		
-		editButtonBox = new HBox();
+
 		chooseButton = new Button("选择图片");
-		chooseButton.setOnAction((event)->{
+		editButtonBox = new HBox();
+		editButtonBox.getChildren().addAll(chooseButton);
+
+		chooseButton.setOnAction((event) -> {
 			FileChooser fileChooser = new FileChooser();
 			fileChooser.setTitle("选择一张图片");
-			fileChooser.getExtensionFilters().addAll(
-					new FileChooser.ExtensionFilter("PNG", "*.png"),
-	                new FileChooser.ExtensionFilter("JPG", "*.jpg")
-	            );
-			
+			fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("PNG", "*.png"),
+					new FileChooser.ExtensionFilter("JPG", "*.jpg"));
+
 			File file = fileChooser.showOpenDialog(getScene().getWindow());
-			
-			if(file==null) return;
-			
+
+			if (file == null)
+				return;
+
 			try {
 				transitionImage = new Image(new FileInputStream(file));
 				FileInputStream fileInputStream = new FileInputStream(file);
@@ -64,23 +65,26 @@ public class ImageEditPane extends VBox{
 				byte[] buf = new byte[1024];
 				int numBytesRead = 0;
 				while ((numBytesRead = fileInputStream.read(buf)) != -1) {
-				output.write(buf, 0, numBytesRead);
+					output.write(buf, 0, numBytesRead);
 				}
 				transitionImageData = output.toByteArray();
-				
+
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			
-			if(transitionImage!=null){
+
+			if (transitionImage != null) {
 				imageView.setImage(transitionImage);
 			}
+			editButtonBox.getChildren().clear();
+			editButtonBox.getChildren().addAll(chooseButton, confirmButton, cancelButton);
+
 		});
-		
+
 		confirmButton = new Button("保存");
-		confirmButton.setOnAction((event)->{
-			if(transitionImage!=null){
+		confirmButton.setOnAction((event) -> {
+			if (transitionImage != null) {
 				preImage = transitionImage;
 				transitionImage = null;
 				imageView.setImage(preImage);
@@ -92,23 +96,24 @@ public class ImageEditPane extends VBox{
 					e.printStackTrace();
 				}
 			}
+			editButtonBox.getChildren().clear();
+			editButtonBox.getChildren().addAll(chooseButton);
+
 		});
-		
+
 		cancelButton = new Button("取消");
-		cancelButton.setOnAction((event)->{
-			if(transitionImage!=null){
+		cancelButton.setOnAction((event) -> {
+			if (transitionImage != null) {
 				imageView.setImage(preImage);
 				transitionImage = null;
 				transitionImageData = null;
 			}
+			editButtonBox.getChildren().clear();
+			editButtonBox.getChildren().addAll(chooseButton);
 		});
-		
-		editButtonBox.getChildren().addAll(chooseButton,confirmButton,cancelButton);
-		
-		this.getChildren().addAll(imageView,editButtonBox);
-		
-		
+
+		this.getChildren().addAll(imageView, editButtonBox);
+
 	}
-	
 
 }
